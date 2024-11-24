53a7064fd6a61e45eacf3fd5c70cc87c1ea88df7
MohamedMahmoud20 <MohamedMahmoud20>
2024-11-24 12:43:17 +0200

    init

--- routes/clients.js
+++ routes/clients.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Clients = require("../models/clients");
 router.get("/" ,async (req,res)=> { 
-    // const clients= await Clients.findAll(
-    //     {
-    //         where : {cln_nam: "رايتو تجربة"}
-    //     }
-    // );
+    const clients= await Clients.findAll(
+        {
+            where : {cln_nam: "رايتو تجربة"}
+        }
+    );
     
     res.status(200).json(clients)
 
