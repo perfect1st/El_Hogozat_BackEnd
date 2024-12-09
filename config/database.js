98543608eeb8fc16fc39fe7ee2f1d989b00f9877
MohamedMahmoud20 <MohamedMahmoud20>
2024-12-09 17:48:30 +0200

    clients

--- config/database.js
+++ config/database.js
@@ -4,13 +4,14 @@ require("dotenv/config");
 module.exports = new Sequelize('ladycareyear012024', 'sa', '123456aA@', {
   host: '158.220.90.76',
   dialect: 'mssql',
-  // dialectOptions: {
-  //   options: {
-  //     encrypt: false, // or true, depending on your setup
-  //     trustServerCertificate: true, // use this for self-signed certificates
-  //   },
-  // },
+  dialectOptions: {
+    options: {
+      encrypt: false,
+      trustServerCertificate: true,
+      enableArithAbort: true
+    }
+  },
   port: 1433,
-  logging : false
+  logging: false
 }
 );
--- routes/clients.js
+++ routes/clients.js
@@ -1,10 +1,106 @@
 const express = require("express");
 const router = express.Router();
 const Clients = require("../models/clients");
-router.get("/" ,async (req,res)=> { 
-    const clients= await Clients.findAll({});
-    
+
+
+
+
+router.get("/", async (req, res) => {
+    const clients = await Clients.findAll({ where: { cln_L4: "824298" } });
     res.status(200).json(clients)
+
+});
+
+
+router.post("/", async (req, res) => {
+
+
+    const { branchId, email, userName, mobile, regNum, birthDate, gender } = req.body;
+    ///////////////////////////////////////////////////////////////////////////////////////////////   
+    let clnKey = "1104";
+    clnKey += (parseInt("0000", 10) + branchId).toString().padStart(4, "0");
+    clnKey += "0001";
+    const lastClient = await Clients.findOne({ order: [['cln_L4', 'DESC']] });
+    let serialNumber = lastClient.cln_L4 + 1;
+    clnKey += (parseInt("000000", 10) + serialNumber).toString().padStart(6, "0");
+    let [day, month, year] = req.body.birthDate.split("-");
+    let date = new Date(Date.UTC(year, month - 1, day, 22, 0, 0, 0));
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    console.log(lastClient)
+
+    const clientData = {
+        cln_L1: 1104, // ثابت
+        cln_L2: branchId, // رقم الفرع
+        cln_L4: serialNumber, // رقم مسلسل
+        cln_email: email,
+        cln_username: userName, // اسم المستخدم
+        cln_birthdate: date, // تاريخ الميلاد
+        cln_RegNum: regNum, // رقم الهويه
+        cln_mobl: mobile, // رقم الهاتف
+        cln_typ: gender, // النوع  ذكر - انثي 
+        cln_date: req.body.birthDate,
+        cln_key: Number(clnKey),
+        cln_nam: userName,
+        cln_enm: userName,
+        cln_prsn: userName,
+        cln_RegName: userName,
+    };
+
+    Clients.create(clientData)
+    .then((data) => {
+      res.status(201).send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err || "حدثت مشكله اثناء عمليه الأضافه",
+      });
+    });
+
+    // res.status(200).json(clientData)
+
+});
+
+router.put("/:id" , async (req,res)=>{
+    const id = req.params.id; 
+   
+    console.log(req.body)
+   
+    try {
+
+        const update = await Clients.update(req.body , { where: { cln_L4: id },  });
+        if(update ==1){
+            res.status(200).send({  message: "تم تعديل بيانات هذا العميل" });
+           }
+           else{
+            console.log(update)
+            res.status(500).send({  message: "لدينا مشكله في عمليه تعديل هذا العميل" });
+           }
+    
+    } catch (error) {
+        res.status(500).send({  message: error.message  });
+    }
+});
+
+router.delete("/:id",async (req,res)=>{
+  
+    const id = req.params.id; 
+      
+    try {
+
+        const update = await Clients.destroy({ where: { cln_L4: id },  });
+        if(update ==1){
+            res.status(200).send({  message: "تم حذف بيانات هذا العميل" });
+           }
+           else{
+            console.log(update)
+            res.status(500).send({  message: "لدينا مشكله في عمليه حذف هذا العميل" });
+        }
+    
+    } catch (error) {
+        res.status(500).send({  message: error.message  });
+    }
+
 
 });
 
